a50642262484bab73b6a090c1e9e277c085539cc
Matthew Miner <matthew.miner89@gmail.com>
2018-11-15 23:05:36 -0600

    Refactor separate class generation functions into one function that will take hash of stats to be modified from base_creature

--- char_gen.rb
+++ char_gen.rb
@@ -1,7 +1,12 @@
 
 require 'json'
 
-# ruby hash but convert to JSON for easy access to battle system
+# ruby hash but convert to JSON for easy access for battle system
+# Ruby doesn't allow for hashes to be "deep" copied without 
+# serialization, as such, I've opted to serialize to JSON 
+# at each step since the goal is to eventually serialize to 
+# JSON
+
 base_creature = {
     :class => "creature",
     :stats => {
@@ -27,89 +32,75 @@ def process_mana(mental)
     return mental * 5
 end
 
-def process_initiative(agility)
-    agility * 10
+def process_initiative(cunning)
+    cunning * 10
 end
 
-# Ruby doesn't allow for hashes to be "deep" copied without 
-# serialization, as such, I've opted to serialize to JSON 
-# at each step since the goal is to eventually serialize to 
-# JSON
+apprentice_hash = {
+    "class" => "apprentice",
+    "stats" => {
+        "mental" => 8,
+        "enmity" => 7
+    }
+}
 
-# scoundrel generates with bonus to cunning, mental, enmity, luck
-# assumes class gets 5 points to distribute
-def create_scoundrel_char(base_stats_json)
-    scoundrel_creature = JSON.parse(base_stats_json)
-    stats_hash = scoundrel_creature["stats"]
-    scoundrel_creature["class"] = "scoundrel"
-    stats_hash["cunning"] += 2
-    stats_hash["mental"] += 1
-    stats_hash["enmity"] += 1
-    stats_hash["luck"] += 1
-    scoundrel_creature["health"] = process_health(stats_hash["vitality"])
-    scoundrel_creature["mana"] = process_mana(stats_hash["mental"])
-    scoundrel_creature["initiative"] = process_initiative(stats_hash["cunning"])
-    # return values of stats nested hash
-    # scoundrel_creature["stats"].values
-    scoundrel_creature.to_json
+brawler_hash = {
+    "class" => "brawler",
+    "stats" => {
+        "might" => 7,
+        "mental" => 6,
+        "vitality" => 6,
+        "resolve" => 6
+    }
+}
+
+initiate_hash = {
+    "class" => "initiate",
+    "stats" => {
+        "mental" => 7,
+        "influence" => 7,
+        "resolve" => 6
+    }
+}
+
+scoundrel_hash = {
+    "class" => "scoundrel",
+    "stats" => {
+        "cunning" => 7,
+        "mental" => 6,
+        "enmity" => 6,
+        "luck" => 1
+    }
+}
+
+def generate_creature(base_stats_json, creature_hash)
+    new_creature = JSON.parse(base_stats_json)
+    
+    new_creature["class"] = creature_hash["class"]
+
+    stats_hash = new_creature["stats"]
+
+    #creature_hash["stats"]
+
+    creature_hash["stats"].each do |stat_name, value|
+        stats_hash["#{stat_name}"] = value
+    end
+
+    new_creature["health"] = process_health(stats_hash["vitality"])
+    new_creature["mana"] = process_mana(stats_hash["mental"])
+    new_creature["initiative"] = process_initiative(stats_hash["cunning"])
+
+    new_creature.to_json
 end
 
 puts
-puts create_scoundrel_char(base_creature)
+puts generate_creature(base_creature, apprentice_hash)
 puts
-
-# brawler generates with bonus to might, mental, vitality, resolve
-# assumes class gets 5 points to distribute
-def create_brawler_char(base_stats_json)
-    brawler_creature = JSON.parse(base_stats_json)
-    brawler_creature["class"] = "brawler"
-    stats_hash = brawler_creature["stats"]
-    stats_hash["might"] += 2
-    stats_hash["mental"] += 1
-    stats_hash["vitality"] += 1
-    stats_hash["resolve"] += 1
-    brawler_creature["health"] = process_health(stats_hash["vitality"])
-    brawler_creature["mana"] = process_mana(stats_hash["mental"])
-    brawler_creature["initiative"] = process_initiative(stats_hash["cunning"])
-    brawler_creature.to_json
-end
-
-puts create_brawler_char(base_creature)
+puts generate_creature(base_creature, brawler_hash)
 puts
-
-# apprentice generates with bonus to mental, influence, resolve
-# assumes class gets 7 points to distribute
-def create_apprentice_char(base_stats_json)
-    apprentice_creature = JSON.parse(base_stats_json)
-    apprentice_creature["class"] = "apprentice"
-    stats_hash = apprentice_creature["stats"]
-    stats_hash["mental"] += 3
-    stats_hash["enmity"] += 2
-    apprentice_creature["health"] = process_health(stats_hash["vitality"])
-    apprentice_creature["mana"] = process_mana(stats_hash["mental"])
-    apprentice_creature["initiative"] = process_initiative(stats_hash["cunning"])
-    apprentice_creature.to_json
-end
-
-puts create_apprentice_char(base_creature)
+puts generate_creature(base_creature, initiate_hash)
 puts
-
-# initiate generates with bonus to agility, cunning, charisma, luck
-# assumes class gets 7 points to distribute
-def create_initiate_char(base_stats_json)
-    initiate_creature = JSON.parse(base_stats_json)
-    initiate_creature["class"] = "initiate"
-    stats_hash = initiate_creature["stats"]
-    stats_hash["mental"] += 2
-    stats_hash["influence"] += 2
-    stats_hash["resolve"] += 1
-    initiate_creature["health"] = process_health(stats_hash["vitality"])
-    initiate_creature["mana"] = process_mana(stats_hash["mental"])
-    initiate_creature["initiative"] = process_initiative(stats_hash["cunning"])
-    initiate_creature.to_json
-end
-
-puts create_initiate_char(base_creature)
+puts generate_creature(base_creature, scoundrel_hash)
 puts
 
 
