2ea2c963a6004f61d771e96efe3598d27b14b568
Matthew Miner <matthew.miner89@gmail.com>
2018-12-01 23:05:04 -0600

    refactor hash syntax and update functions to reflect changes

--- char_gen.rb
+++ char_gen.rb
@@ -6,57 +6,59 @@ require 'json'
 # at each step since the goal is to eventually serialize to 
 # JSON
 
+isolated_stats = {
+        might: 5,
+        cunning: 5,
+        mental: 5,
+        influence: 5,
+        vitality: 5,
+        resolve: 5,
+        enmity: 5,
+        luck: 0
+}
+
 base_creature = {
     :class => "creature",
-    :stats => {
-        :might => 5,
-        :cunning => 5,
-        :mental => 5,
-        :influence => 5,
-        :vitality => 5,
-        :resolve => 5,
-        :enmity => 5,
-        :luck => 0
-    },
+    :stats => isolated_stats,
     :health => 25,
     :mana => 15,
     :initiative => 50
-}.to_json
+}
 
 apprentice = {
-    "class" => "apprentice",
-    "stats" => {
-        "mental" => 8,
-        "enmity" => 7
+    :class => "apprentice",
+    :stats => {
+        :mental => 8,
+        :enmity => 7
     }
 }
 
 brawler = {
-    "class" => "brawler",
-    "stats" => {
-        "might" => 7,
-        "mental" => 6,
-        "vitality" => 6,
-        "resolve" => 6
+    :class => "brawler",
+    :stats => {
+        :might => 7,
+        :mental => 6,
+        :vitality => 6,
+        :resolve => 6
     }
 }
 
 initiate = {
-    "class" => "initiate",
-    "stats" => {
-        "mental" => 7,
-        "influence" => 7,
-        "resolve" => 6
+    :class => "initiate",
+    :stats => {
+        :mental => 7,
+        :influence => 7,
+        :resolve => 6
     }
 }
 
 scoundrel = {
-    "class" => "scoundrel",
-    "stats" => {
-        "cunning" => 7,
-        "mental" => 6,
-        "enmity" => 6,
-        "luck" => 1
+    :class => "scoundrel",
+    :stats => {
+        :cunning => 7,
+        :mental => 6,
+        :enmity => 6,
+        :luck => 1
     }
 }
 
@@ -73,22 +75,20 @@ def process_initiative(cunning)
 end
 
 def process_dependent_stats(new_creature, stats)
-    new_creature["health"] = process_health(stats["vitality"])
-    new_creature["mana"] = process_mana(stats["mental"])
-    new_creature["initiative"] = process_initiative(stats["cunning"])
+    new_creature[:health] = process_health(stats[:vitality])
+    new_creature[:mana] = process_mana(stats[:mental])
+    new_creature[:initiative] = process_initiative(stats[:cunning])
 end
 
 def generate_creature(base_stats, creature)
-    new_creature = JSON.parse(base_stats)
-    
-    new_creature["class"] = creature["class"]
+    new_creature = base_stats
 
-    stats = new_creature["stats"]
+    new_creature[:class] = creature[:class]
 
-    #creature["stats"]
+    stats = new_creature[:stats]
 
-    creature["stats"].each do |stat_name, value|
-        stats["#{stat_name}"] = value
+    creature[:stats].each do |stat_name, value|
+        stats[stat_name.to_sym] = value
     end
 
     process_dependent_stats(new_creature, stats)
