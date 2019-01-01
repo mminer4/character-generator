f538d8d3fe642d16e24c45843775bfe951578455
Matthew Miner <matthew.miner89@gmail.com>
2019-01-01 14:23:26 -0600

    Wrap character generation in a class and separate static class stats into module

--- char_gen.rb
+++ char_gen.rb
@@ -1,80 +1,63 @@
 require 'json'
 require_relative 'base_creature'
-require_relative 'male_names'
+require_relative 'names_collection'
+require_relative 'class_stats'
 
-apprentice = {
-    class: "apprentice",
-    stats: {
-        mental: 8,
-        enmity: 7
-    }
-}
+class CharacterGenerator
 
-brawler = {
-    class: "brawler",
-    stats: {
-        might: 7,
-        mental: 6,
-        vitality: 6,
-        resolve: 6
-    }
-}
-
-initiate = {
-    class: "initiate",
-    stats: {
-        mental: 7,
-        influence: 7,
-        resolve: 6
-    }
-}
-
-scoundrel = {
-    class: "scoundrel",
-    stats: {
-        cunning: 7,
-        mental: 6,
-        enmity: 6,
-        luck: 1
-    }
-}
-
-def process_health(vitality)
-    vitality * 5
-end
-
-def process_mana(mental)
-    mental * 5
-end
-
-def process_dependent_stats(new_creature, stats)
-    new_creature[:health] = process_health(stats[:vitality])
-    new_creature[:mana] = process_mana(stats[:mental])
-end
-
-def select_name
-    names_collection = NamesCollection::MALE_NAMES
-    names_collection.sample
-end
-
-def create_base_creature
-    base_creature = BaseCreature.new
-end
-
-def generate_creature(creature)
-
-    new_creature = create_base_creature.info
-
-    new_creature[:class] = creature[:class]
-    new_creature[:name] = select_name
-
-    stats = new_creature[:stats]
-
-    creature[:stats].each do |stat_name, value|
-        stats[stat_name] = value
+    def process_health(vitality)
+        vitality * 5
     end
 
-    process_dependent_stats(new_creature, stats)
+    def process_mana(mental)
+        mental * 5
+    end
 
-    new_creature.to_json
+    def process_dependent_stats(new_creature, stats)
+        new_creature[:health] = process_health(stats[:vitality])
+        new_creature[:mana] = process_mana(stats[:mental])
+    end
+
+    def select_name
+        names_collection = NamesCollection::MALE_NAMES
+        names_collection.sample
+    end
+
+    def create_base_creature
+        base_creature = BaseCreature.new
+    end
+
+    def generate_creature(creature)
+
+        new_creature = create_base_creature.info
+
+        new_creature[:class] = creature[:class]
+        new_creature[:name] = select_name
+
+        stats = new_creature[:stats]
+
+        creature[:stats].each do |stat_name, value|
+            stats[stat_name] = value
+        end
+
+        process_dependent_stats(new_creature, stats)
+
+        new_creature.to_json
+    end
+
+    def make_apprentice
+        generate_creature(ClassStats::APPRENTICE)
+    end
+
+    def make_brawler
+        generate_creature(ClassStats::BRAWLER)
+    end
+
+    def make_initiate
+        generate_creature(ClassStats::INITIATE)
+    end
+
+    def make_scoundrel
+        generate_creature(ClassStats::SCOUNDREL)
+    end
 end
---
+++ class_stats.rb
@@ -0,0 +1,38 @@
+module ClassStats
+    APPRENTICE = {
+        class: "apprentice",
+        stats: {
+            mental: 8,
+            enmity: 7
+        }
+    }
+
+    BRAWLER = {
+        class: "brawler",
+        stats: {
+            might: 7,
+            mental: 6,
+            vitality: 6,
+            resolve: 6
+        }
+    }
+
+    INITIATE = {
+        class: "initiate",
+        stats: {
+            mental: 7,
+            influence: 7,
+            resolve: 6
+        }
+    }
+
+    SCOUNDREL = {
+        class: "scoundrel",
+        stats: {
+            cunning: 7,
+            mental: 6,
+            enmity: 6,
+            luck: 1
+        }
+    }
+end
